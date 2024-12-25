ebc8d11df10e80cbc18a3aae453308cab27cd190
venexene <venexene@gmail.com>
2024-12-25 19:12:42 +0300

    Add Ground

--- OpenGL/lab14/main.cpp
+++ OpenGL/lab14/main.cpp
@@ -14,6 +14,7 @@ Model model0;
 Model model1;
 Model model2;
 Model tree_model;
+Model ground_model;
 GLuint instanceVBO;
 
 const std::string model_path = "data/Cearadactylus.obj";
@@ -24,6 +25,8 @@ const std::string model_path2 = "data/Triceratops.obj";
 const std::string texture_path2 = "data/Triceratops.png";
 const std::string model_path3 = "data/treeBirch.obj";
 const std::string texture_path3 = "data/treeBirch.jpg";
+const std::string model_path4 = "data/ground.obj";
+const std::string texture_path4 = "data/ground.jpg";
 
 enum class light_kind {PointLightSource, Spotlight, DirLightSource};
 constexpr light_kind LIGHT_KIND = light_kind::PointLightSource;
@@ -108,6 +111,7 @@ void Init() {
 	model1 = Model(model_path1, texture_path1);
 	model2 = Model(model_path2, texture_path2);
 	tree_model = Model(model_path3, texture_path3);
+	ground_model = Model(model_path4, texture_path4);
 	glm::mat4 model = glm::rotate(glm::mat4(1.0f), glm::radians(45.0f), glm::vec3(1.0f, 1.0f, 0.0f));
 	glm::mat4 view = glm::translate(glm::mat4(1.0f), glm::vec3(0.0f, 0.0f, -5.0f));
 	glm::mat4 projection = glm::perspective(glm::radians(45.0f), 1.0f, 0.1f, 100.0f);
@@ -146,7 +150,7 @@ void Draw() {
 	glUniformMatrix3fv(glGetUniformLocation(Program, "transform.normal"), 1, GL_FALSE, glm::value_ptr(normalMatrix));
 	model0.display_model(Program);
 
-	model = glm::translate(glm::mat4(1.0f), glm::vec3(-50.0f, -30.0f, 30.0f));
+	model = glm::translate(glm::mat4(1.0f), glm::vec3(-50.0f, 25.0f, 30.0f));
 	model = glm::scale(model, glm::vec3(0.1f, 0.1f, 0.1f));
 	model = glm::rotate(model, glm::radians(90.0f), glm::vec3(-1.0f, 0.0f, 1.0f));
 	normalMatrix = glm::transpose(glm::inverse(glm::mat3(model)));
@@ -210,6 +214,13 @@ void Draw() {
 	glUniformMatrix4fv(glGetUniformLocation(Program, "transform.model"), 1, GL_FALSE, glm::value_ptr(model));
 	glUniformMatrix3fv(glGetUniformLocation(Program, "transform.normal"), 1, GL_FALSE, glm::value_ptr(normalMatrix));
 	model0.display_model(Program);
+
+	model = glm::translate(glm::mat4(1.0f), glm::vec3(30.0f, 0.0f, 35.0f));
+	model = glm::scale(model, glm::vec3(100.0f, 100.0f, 100.0f));
+	normalMatrix = glm::transpose(glm::inverse(glm::mat3(model)));
+	glUniformMatrix4fv(glGetUniformLocation(Program, "transform.model"), 1, GL_FALSE, glm::value_ptr(model));
+	glUniformMatrix3fv(glGetUniformLocation(Program, "transform.normal"), 1, GL_FALSE, glm::value_ptr(normalMatrix));
+	ground_model.display_model(Program);
 
 	glUniformMatrix4fv(glGetUniformLocation(Program, "transform.viewProjection"), 1, GL_FALSE, glm::value_ptr(projection * view));
 	glUniform3fv(glGetUniformLocation(Program, "transform.viewPosition"), 1, glm::value_ptr(cameraPos));
